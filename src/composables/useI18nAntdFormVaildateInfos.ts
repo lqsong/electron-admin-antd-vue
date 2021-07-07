5478ebab6736e2b2502f787bc236fdc7260482ec
李庆松 <lqs@ecrrc.com>
2021-07-07 18:51:00 +0800

    fix from , remove @ant-design-vue/use

--- src/composables/useI18nAntdFormVaildateInfos.ts
+++ src/composables/useI18nAntdFormVaildateInfos.ts
@@ -2,23 +2,23 @@
  * 重置 Antd Form VaildateInfos 为 I18n
  * @author LiQingSong
  */
-import { computed } from 'vue';
-import { useI18n } from 'vue-i18n';
-import { validateInfos } from '@ant-design-vue/use/lib/useForm';
-
-export default function useI18nAntdFormVaildateInfos(infos: validateInfos): validateInfos {
-    const{ t } = useI18n();
-
-    const infosNew = computed<validateInfos>(() => {
-        const vInfos: validateInfos  = {};            
-        for (const index in infos) {
-            vInfos[index] = JSON.parse(JSON.stringify(infos[index]));
-            if(vInfos[index] && vInfos[index]['help']) {
-                vInfos[index]['help'] = t(vInfos[index]['help'] || '');
-            }
-        }
-        return vInfos;
-    });
-
-    return infosNew as unknown as validateInfos;
-}
+ import { computed } from 'vue';
+ import { useI18n } from 'vue-i18n';
+ import { validateInfos } from 'ant-design-vue/lib/form/useForm';
+ 
+ export default function useI18nAntdFormVaildateInfos(infos: validateInfos): validateInfos {
+     const{ t } = useI18n();
+ 
+     const infosNew = computed<validateInfos>(() => {
+         const vInfos: validateInfos  = {};  
+         for (const index in infos) {
+             vInfos[index] = JSON.parse(JSON.stringify(infos[index]));           
+             if(vInfos[index] && vInfos[index]['help']) {
+                 vInfos[index]['help'] = vInfos[index]['help'].map((item: any)=> typeof(item)=='string' ? t(item) : item.map((item2: any)=> item2 ? t(item2):''));
+             }
+         }
+         return vInfos;
+     });
+ 
+     return infosNew as unknown as validateInfos;
+ }
